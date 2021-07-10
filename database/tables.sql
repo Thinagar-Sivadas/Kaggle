dda6c1c8c2673a17b2d650f0b84b4c83a2d2984c
Thinagar <thinagarsivadas@gmail.com>
2021-07-10 17:33:59 +0800

    updated database

--- database/tables.sql
+++ database/tables.sql
@@ -1,6 +1,4 @@
 -- Create Table
-USE {database};
-
 DECLARE @table NVARCHAR(MAX);
 SET @table = 'STATEWISE_DATA';
 
@@ -15,6 +13,30 @@ SET @sql = '
 		Active_Ratio FLOAT(24),
 		Discharge_Ratio FLOAT(24),
 		Death_Ratio FLOAT(24)
+		);';
+
+IF OBJECT_ID('{database}.{schema}.' + @table) IS NOT NULL
+	BEGIN
+		PRINT 'Database table ' + @table + ' exists';
+	END
+ELSE
+    BEGIN
+		EXEC(@sql);
+		PRINT 'Database table ' + @table + ' created';
+	END
+
+GO
+
+DECLARE @table NVARCHAR(MAX);
+SET @table = 'STATEWISE_VACCINE_DATA';
+
+DECLARE @sql NVARCHAR(MAX);
+SET @sql = '
+		CREATE TABLE {database}.{schema}.' + @table +' (
+		State_UTs VARCHAR(MAX),
+		DOSE_1 INT,
+        DOSE_2 INT,
+		TOTAL_VACCINE_DOSE INT,
 		);';
 
 IF OBJECT_ID('{database}.{schema}.' + @table) IS NOT NULL
