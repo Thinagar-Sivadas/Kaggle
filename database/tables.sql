4210382d0561b66bad805f6aab3ed26ee3ff049f
Thinagar <thinagarsivadas@gmail.com>
2021-09-25 23:30:35 +0800

    update db

--- database/tables.sql
+++ database/tables.sql
@@ -51,3 +51,40 @@ ELSE
 		EXEC(@sql);
 		PRINT 'Database table ' + @table + ' created';
 	END
+
+GO
+
+DECLARE @table NVARCHAR(MAX);
+SET @table = 'SUICIDE';
+
+DECLARE @sql NVARCHAR(MAX);
+SET @sql = '
+		CREATE TABLE {database}.{schema}.' + @table +' (
+		ID INT IDENTITY(1,1) PRIMARY KEY,
+		State VARCHAR(MAX),
+		Year INT,
+		Cause VARCHAR(MAX),
+		Male_14 INT,
+		Male_15_29 INT,
+		Male_30_44 INT,
+		Male_45_59 INT,
+		Male_60 INT,
+		Total_Male INT,
+		Female_14 INT,
+		Female_15_29 INT,
+		Female_30_44 INT,
+		Female_45_59 INT,
+		Female_60 INT,
+		Total_Female INT,
+		Total_Male_Female INT,
+		);';
+
+IF OBJECT_ID('{database}.{schema}.' + @table) IS NOT NULL
+	BEGIN
+		PRINT 'Database table ' + @table + ' exists';
+	END
+ELSE
+    BEGIN
+		EXEC(@sql);
+		PRINT 'Database table ' + @table + ' created';
+	END
