60cbc6696d42f38fccf6456ca79b6c8e223f8186
Thinagar <thinagarsivadas@gmail.com>
2021-10-08 20:37:43 +0800

    add covid script

--- database/tables.sql
+++ database/tables.sql
@@ -28,18 +28,19 @@ ELSE
 GO
 
 DECLARE @table NVARCHAR(MAX);
-SET @table = 'INDIAN_IMDB';
+SET @table = 'COVID_19';
 
 DECLARE @sql NVARCHAR(MAX);
 SET @sql = '
 		CREATE TABLE {database}.{schema}.' + @table +' (
-		ID INT IDENTITY(1,1) PRIMARY KEY,
-		Title VARCHAR(MAX),
-		Year_Of_Release SMALLINT,
-		Genre VARCHAR(MAX),
-		Director VARCHAR(MAX),
-		Number_Of_Votes INT,
-		IMDB_Rating FLOAT
+		continent VARCHAR(MAX),
+		country VARCHAR(MAX),
+		date Date,
+		total_cases INT,
+		new_cases INT,
+		total_deaths INT,
+		new_deaths INT,
+		icu_patients INT,
 		);';
 
 IF OBJECT_ID('{database}.{schema}.' + @table) IS NOT NULL
